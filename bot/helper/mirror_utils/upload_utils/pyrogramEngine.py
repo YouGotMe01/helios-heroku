8a0bbacf8fdcc92b1de5fc13c323aa19cb958b39
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-05-07 12:06:03 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -1,4 +1,4 @@
-from telegram bot import ParseMode
+   from telegram bot import ParseMode
 from logging import getLogger, ERROR
 from os import remove as osremove, walk, path as ospath, rename as osrename
 from time import time, sleep
