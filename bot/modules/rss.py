45ba85747b2891dac56b6a1bebd8f51a90a51544
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-05-29 21:57:44 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -14,7 +14,7 @@ from bot.helper.ext_utils.db_handler import DbManger
 from bot.helper.telegram_helper import button_build
 
 rss_dict_lock = Lock()
-magnets = []
+torrents = []
 
 def rss_list(update, context):
     if len(rss_dict) > 0:
@@ -230,10 +230,10 @@ def rss_monitor(context):
                     soup4=BeautifulSoup(lmno,'html.parser')
                     for pqrs in soup4.find_all('a',attrs={'href':re.compile(r"^torrend")}): 
                         url=pqrs.get('href')
-                        if url in magnets:
+                        if url in torrents:
                             break
                         else: 
-                            magnets.append(url)
+                            torrents.append(url)
                         feed_msg = f"/{RSS_COMMAND} {url}"
                         sendRss(feed_msg, context.bot)
                 else:
