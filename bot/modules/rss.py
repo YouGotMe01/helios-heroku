d905f147eef1a8846a430ecec650c4dea64a49a9
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-05-31 12:01:24 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -228,7 +228,7 @@ def rss_monitor(context):
                     scraper = cloudscraper.create_scraper(allow_brotli=False)
                     lmno=scraper.get(hijk).text
                     soup4=BeautifulSoup(lmno,'html.parser')
-                    for pqrs in soup4.find_all('a',attrs={'href':re.compile(r"^/download-")}): 
+                    for pqrs in soup4.find_all('a',attrs={'href':re.compile(r"^.torrent")}): 
                         torrent_links=pqrs.get('href')
                         if torrent_links in torrents:
                             break
