792b558f4b246722fb02f34057e395c5e08dcbb8
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-06-10 09:35:29 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -187,7 +187,7 @@ def rss_set_update(update, context):
         except:
             pass
         
-urls = url
+Urls = Feed_url
 def rss_monitor(context):
     with rss_dict_lock:
         if len(rss_dict) == 0:
