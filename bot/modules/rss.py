a476226a79ee650494fb8584d4fcb0c140edcd23
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-06-11 23:01:08 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -249,7 +249,7 @@ def rss_monitor(context):
                         feed_msg = f"<b>Name: </b><code>{rss_d.entries[feed_count]['title'].replace('>', '').replace('<', '')}</code>\n\n"
                         feed_msg += f"<b>Link: </b><code>{url}</code>"
                         SentRss(feed_msg, context.bot)
-                    feed_count += 
+                    feed_count += 1
                     sleep(5)
             DbManger().rss_update(name, str(last_link), str(last_title))
             with rss_dict_lock:
