a9750b7e2acffa02c21c513de348837288da7dab
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-06-13 12:28:30 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -263,19 +263,19 @@ def rss_monitor(context):
                         LOGGER.error(f"{e} Feed Name: {name} - Feed Link: {data[0]}")
                         continue
 
-   if DB_URI is not None and RSS_CHAT_ID is not None:
-       rss_list_handler = CommandHandler(BotCommands.RssListCommand, rss_list, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-       rss_get_handler = CommandHandler(BotCommands.RssGetCommand, rss_get, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-       rss_sub_handler = CommandHandler(BotCommands.RssSubCommand, rss_sub, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-       rss_unsub_handler = CommandHandler(BotCommands.RssUnSubCommand, rss_unsub, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-       rss_settings_handler = CommandHandler(BotCommands.RssSettingsCommand, rss_settings, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-       rss_buttons_handler = CallbackQueryHandler(rss_set_update, pattern="rss", run_async=True)
+if DB_URI is not None and RSS_CHAT_ID is not None:
+    rss_list_handler = CommandHandler(BotCommands.RssListCommand, rss_list, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    rss_get_handler = CommandHandler(BotCommands.RssGetCommand, rss_get, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    rss_sub_handler = CommandHandler(BotCommands.RssSubCommand, rss_sub, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    rss_unsub_handler = CommandHandler(BotCommands.RssUnSubCommand, rss_unsub, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    rss_settings_handler = CommandHandler(BotCommands.RssSettingsCommand, rss_settings, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    rss_buttons_handler = CallbackQueryHandler(rss_set_update, pattern="rss", run_async=True)
     
-       dispatcher.add_handler(rss_list_handler)
-       dispatcher.add_handler(rss_get_handler)
-       dispatcher.add_handler(rss_sub_handler)
-       dispatcher.add_handler(rss_unsub_handler)
-       dispatcher.add_handler(rss_settings_handler)
-       dispatcher.add_handler(rss_buttons_handler)
-       rss_job = job_queue.run_repeating(rss_monitor, interval=RSS_DELAY, first=20, name="RSS")
-       rss_job.enabled = True
+    dispatcher.add_handler(rss_list_handler)
+    dispatcher.add_handler(rss_get_handler)
+    dispatcher.add_handler(rss_sub_handler)
+    dispatcher.add_handler(rss_unsub_handler)
+    dispatcher.add_handler(rss_settings_handler)
+    dispatcher.add_handler(rss_buttons_handler)
+    rss_job = job_queue.run_repeating(rss_monitor, interval=RSS_DELAY, first=20, name="RSS")
+    rss_job.enabled = True
