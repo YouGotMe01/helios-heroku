d32ccf635fbf1c7c670c3c7d4716ed295e39b1df
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-06-19 10:40:27 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -3,7 +3,6 @@ import requests
 import feedparser
 import hashlib
 import bencodepy
-import libtorrent as lt
 from bs4 import BeautifulSoup
 from time import sleep
 from telegram.ext import CommandHandler, CallbackQueryHandler
