6b31b8effb66fd90b5ed5c6370f2774906344d50
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-06-22 17:45:30 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -15,7 +15,8 @@ from dotenv import load_dotenv
 from pyrogram import Client, enums
 from asyncio import get_event_loop
 from megasdkrestclient import MegaSdkRestClient, errors as mega_err
-from psycopg2 import DbManager
+from bot import DbManager
+
 
 main_loop = get_event_loop()
 
