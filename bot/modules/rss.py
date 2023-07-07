eac28829bcaf12d2a3a1ea0bcfc5100f5301b35d
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-07-07 14:36:15 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -1,4 +1,5 @@
 import os
+import functools
 import logging
 import threading
 import psycopg2
@@ -316,5 +317,10 @@ if DB_URI is not None and RSS_CHAT_ID is not None:
     dispatcher.add_handler(rss_unsub_handler)
     dispatcher.add_handler(rss_settings_handler)
     dispatcher.add_handler(rss_buttons_handler)
-    rss_job = job_queue.run_repeating(rss_monitor, interval=RSS_DELAY, first=20, name="RSS")
-    rss_job.enabled = True
+    rss_job = job_queue.run_repeating(
+        functools.partial(rss_monitor, db_url=db_url),
+        interval=RSS_DELAY,
+        first=20,
+        name="RSS")
+
+
