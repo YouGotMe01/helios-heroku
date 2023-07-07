fa053aade799b9c5f8afea3a9d7a855da4acaea7
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-07-07 16:05:32 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -61,8 +61,6 @@ def rss_get(update, context):
     except (IndexError, ValueError):
         sendMessage(f"Use this format to fetch:\n/{BotCommands.RssGetCommand} Title value", context.bot, update.message)
         
-import feedparser
-
 def rss_sub(update, context):
     try:
         args = update.message.text.split(maxsplit=3)
@@ -103,9 +101,10 @@ def rss_sub(update, context):
             last_link = str(rss_d.entries[0]['link'])
             last_title = str(rss_d.entries[0]['title'])
 
+            # Add the modified code here to populate rss_dict with the correct data structure
             rss_dict_lock.acquire()
             try:
-                rss_dict[title] = {'url': feed_link, 'title': feed_title}
+                rss_dict[title] = {'url': feed_link, 'title': rss_d.feed.title}
             finally:
                 rss_dict_lock.release()
 
@@ -114,18 +113,18 @@ def rss_sub(update, context):
             LOGGER.info(f"Rss Feed Added: {title} - {feed_link} - {filters}")
         except (IndexError, AttributeError) as e:
             LOGGER.error(str(e))
-            msg = "The link doesn't seem to be an RSS feed or it's region-blocked!"
+            msg = "The link doesn't seem to be a valid RSS feed or it's region-blocked!"
             sendMessage(msg, context.bot, update.message)
         except Exception as e:
             LOGGER.error(str(e))
             sendMessage(str(e), context.bot, update.message)
     except IndexError:
-        msg = f"Use this format to add feed url:\n/{BotCommands.RssSubCommand} Title https://www.rss-url.com"
-        msg += " f: 1080 or 720 or 144p|mkv or mp4|hevc (optional)\n\nThis filter will parse links that its titles"
-        msg += " contain `(1080 or 720 or 144p) and (mkv or mp4) and hevc` words. You can add whatever you want.\n\n"
-        msg += "Another example: f:  1080  or 720p|.web. or .webrip.|hvec or x264. This will parse titles that contain"
+        msg = f"Use this format to add a feed URL:\n/{BotCommands.RssSubCommand} Title https://www.rss-url.com"
+        msg += " f: 1080 or 720 or 144p|mkv or mp4|hevc (optional)\n\nThis filter will parse links that have titles"
+        msg += " containing `(1080 or 720 or 144p) and (mkv or mp4) and hevc` words. You can add whatever you want.\n\n"
+        msg += "Another example: f:  1080  or 720p|.web. or .webrip.|hvec or x264. This will parse titles that have"
         msg += " (1080 or 720p) and (.web. or .webrip.) and (hevc or x264). I have added space before and after 1080"
-        msg += " to avoid wrong matching. If there's a `10805695` number in the title, it will match 1080 if added 1080"
+        msg += " to avoid wrong matching. If there's a `10805695` number in the title, it will match 1080 if you added 1080"
         msg += " without spaces after it."
         msg += "\n\nFilters Notes:\n\n1. | means and.\n\n2. Add `or` between similar keys, you can add it"
         msg += " between qualities or between extensions, so don't add filters like this f: 1080|mp4 or 720|web"
@@ -134,7 +133,6 @@ def rss_sub(update, context):
         msg += "\n\n4. Take a look at the title if it has static special characters after or before the qualities or extensions"
         msg += " or whatever and use them in filters to avoid wrong matches."
         sendMessage(msg, context.bot, update.message)
-
 
 def rss_unsub(update, context):
     try:
