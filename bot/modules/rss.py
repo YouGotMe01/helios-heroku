6436ea1ea75bd685db01dc448d421deeb74b0551
Teamwdl02 <116863191+Teamwdl02@users.noreply.github.com>
2023-07-08 10:51:28 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -1,16 +1,5 @@
-import os
-import logging
-import threading
-import psycopg2
-import cloudscraper
-import re
-import feedparser
-import time
-import hashlib
-from bs4 import BeautifulSoup
-from functools import partial
+from feedparser import parse as feedparse
 from time import sleep
-from psycopg2 import DatabaseError
 from telegram.ext import CommandHandler, CallbackQueryHandler
 from threading import Lock, Thread
 
@@ -40,7 +29,7 @@ def rss_get(update, context):
         if feed_url is not None and count > 0:
             try:
                 msg = sendMessage(f"Getting the last <b>{count}</b> item(s) from {title}", context.bot, update.message)
-                rss_d = feedparser.parse(feed_url)
+                rss_d = feedparse(feed_url[0])
                 item_info = ""
                 for item_num in range(count):
                     try:
@@ -57,10 +46,10 @@ def rss_get(update, context):
                 LOGGER.error(str(e))
                 editMessage(str(e), msg)
         else:
-            sendMessage("Enter a valid title/value.", context.bot, update.message)
+            sendMessage("Enter a vaild title/value.", context.bot, update.message)
     except (IndexError, ValueError):
         sendMessage(f"Use this format to fetch:\n/{BotCommands.RssGetCommand} Title value", context.bot, update.message)
-        
+
 def rss_sub(update, context):
     try:
         args = update.message.text.split(maxsplit=3)
@@ -86,7 +75,7 @@ def rss_sub(update, context):
             LOGGER.error("This title already subscribed! Choose another title!")
             return sendMessage("This title already subscribed! Choose another title!", context.bot, update.message)
         try:
-            rss_d = feedparser.parse(feed_link)
+            rss_d = feedparse(feed_link)
             sub_msg = "<b>Subscribed!</b>"
             sub_msg += f"\n\n<b>Title: </b><code>{title}</code>\n<b>Feed Url: </b>{feed_link}"
             sub_msg += f"\n\n<b>latest record for </b>{rss_d.feed.title}:"
@@ -99,7 +88,7 @@ def rss_sub(update, context):
             sub_msg += f"\n\n<b>Filters: </b><code>{filters}</code>"
             last_link = str(rss_d.entries[0]['link'])
             last_title = str(rss_d.entries[0]['title'])
-            DbManger().rss_add(title, feed_link, last_link, last_title, filters)
+            DbManager().rss_add(title, feed_link, last_link, last_title, filters)
             with rss_dict_lock:
                 if len(rss_dict) == 0:
                     rss_job.enabled = True
@@ -138,7 +127,7 @@ def rss_unsub(update, context):
             LOGGER.error(msg)
             sendMessage(msg, context.bot, update.message)
         else:
-            DbManger().rss_delete(title)
+            DbManager().rss_delete(title)
             with rss_dict_lock:
                 del rss_dict[title]
             sendMessage(f"Rss link with Title: <code>{title}</code> has been removed!", context.bot, update.message)
@@ -170,7 +159,7 @@ def rss_set_update(update, context):
     elif data[1] == 'unsuball':
         query.answer()
         if len(rss_dict) > 0:
-            db_manager.trunc_table('rss')
+            DbManager().trunc_table('rss')
             with rss_dict_lock:
                 rss_dict.clear()
             rss_job.enabled = False
@@ -195,134 +184,57 @@ def rss_set_update(update, context):
             query.message.reply_to_message.delete()
         except:
             pass
-            
-LOGGER = logging.getLogger(__name__)
-db_url = os.environ.get('DATABASE_URL')
 
-class DbManager:
-    def __init__(self, db_url):
-        self.db_url = db_url
-
-    def get_connection(self):
-        return psycopg2.connect(self.db_url)
-
-    def execute_query(self, query, parameters=None):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                if parameters:
-                    cur.execute(query, parameters)
-                else:
-                    cur.execute(query)
-
-    def rss_update(self, name, feed_url, entry_link, entry_title, last_title):
-        query = "UPDATE rss_data SET last_processed_entry = %s, last_title = %s WHERE name = %s AND feed_url = %s"
-        parameters = (entry_link, entry_title, name, feed_url)
-        self.execute_query(query, parameters)
-
-    def rss_delete(self, name):
-        query = "DELETE FROM rss_data WHERE name = %s"
-        parameters = (name,)
-        self.execute_query(query, parameters)
-
-    def rss(self, name, feed_url, feed_title):
-        query = "INSERT INTO rss_data (name, feed_url, feed_title) VALUES (%s, %s, %s)"
-        parameters = (name, feed_url, feed_title)
-        self.execute_query(query, parameters)
-
-    def get_last_processed_entry(self, name, feed_url):
-        query = "SELECT last_processed_entry FROM rss_data WHERE name = %s AND feed_url = %s"
-        parameters = (name, feed_url)
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(query, parameters)
-                row = cur.fetchone()
-                if row:
-                    return row[0]
-                return None
-
-def rss_monitor(update, context, db_url):
-    db_manager = DbManager(db_url)
-
+def rss_monitor(context):
     with rss_dict_lock:
-        rss_saver = rss_dict.copy()
-        print(list(rss_saver.keys()))
-
-    for name, data_list in rss_saver.items():
+        if len(rss_dict) == 0:
+            rss_job.enabled = False
+            return
+        rss_saver = rss_dict
+    for name, data in rss_saver.items():
         try:
-            for data in data_list:
-                if not isinstance(data, dict) or 'url' not in data or 'title' not in data:
-                    LOGGER.warning(f"Invalid data structure for feed: {name}")
+            rss_d = feedparse(data[0])
+            last_link = rss_d.entries[0]['link']
+            last_title = rss_d.entries[0]['title']
+            if data[1] == last_link or data[2] == last_title:
+                continue
+            feed_count = 0
+            while True:
+                try:
+                    if data[1] == rss_d.entries[feed_count]['link'] or data[2] == rss_d.entries[feed_count]['title']:
+                        break
+                except IndexError:
+                    LOGGER.warning(f"Reached Max index no. {feed_count} for this feed: {name}. \
+                          Maybe you need to use less RSS_DELAY to not miss some torrents")
+                    break
+                parse = True
+                for list in data[3]:
+                    if not any(x in str(rss_d.entries[feed_count]['title']).lower() for x in list):
+                        parse = False
+                        feed_count += 1
+                        break
+                if not parse:
                     continue
-
-                feed_url = data['url']
-                feed_title = data['title']
-
-                if feed_url is None:
-                    LOGGER.warning(f"No feed URL available for feed: {name}")
-                    continue
-
-                with db_manager.get_connection() as conn, conn.cursor() as cur:
-                    cur.execute("SELECT last_title, feed_url FROM rss_data WHERE name = %s", (name,))
-                    row = cur.fetchone()
-                    last_title = row[0] if row else None
-                    db_feed_url = row[1] if row else None
-
-                print(f"Feed: {name}")
-                print(f"Feed URL: {feed_url}")
-                print(f"DB Feed URL: {db_feed_url}")
-
-                if db_feed_url != feed_url:
-                    db_manager.rss_delete(name)
-                    db_manager.rss(name, feed_url, feed_title)
-
-                rss_d = feedparser.parse(feed_url)
-                if not rss_d.entries:
-                    LOGGER.warning(f"No entries found for feed: {name} - Feed URL: {feed_url}")
-                    continue  # Skip processing this feed if no entries are found
-
-                magnets = set()
-                cur_last_title = None
-                for entry in rss_d.entries:
-                    entry_link = entry.get('link')
-                    entry_title = entry.get('title')
-                    entry_id = entry.get('id')
-                    last_processed_entry = db_manager.get_last_processed_entry(name, feed_url)
-                    if last_processed_entry == entry_link:
-                        LOGGER.info(f"Entry already processed for feed: {name}")
-                        continue
-
-                    identifier = hashlib.md5(f"{feed_url}-{entry_link}".encode()).hexdigest()
-
-                    if RSS_COMMAND is not None:
-                        magnet_url = entry_link
-                        scraper = cloudscraper.create_scraper(allow_brotli=False)
-                        html = scraper.get(magnet_url).text
-                        soup = BeautifulSoup(html, 'html.parser')
-                        for a_tag in soup.find_all('a', attrs={'href': re.compile(r"^magnet")}):
-                            magnet_url = a_tag.get('href')
-                            title = entry_title.replace('>', '').replace('<', '')
-                            if (magnet_url, title) not in magnets:
-                                magnets.add((magnet_url, title))
-                        for magnet_url, title in magnets:
-                            feed_msg = f"/{RSS_COMMAND} {magnet_url}"
-                            sendRss(feed_msg, context.bot)
-                    else:
-                        feed_msg = f"<b>Name: </b><code>{entry_title.replace('>', '').replace('<', '')}</code>\n\n"
-                        feed_msg += f"<b>Link: </b><code>{entry_link}</code>"
-                        sendRss(feed_msg, context.bot)
-
-                    if cur_last_title is None:
-                        db_manager.rss_update(name, feed_url, entry_link, entry_title, last_title)
-                        cur_last_title = entry_title
-                    else:
-                        db_manager.rss_update(name, feed_url, entry_link, entry_title, cur_last_title)
-                        cur_last_title = entry_title
-
+                try:
+                    url = rss_d.entries[feed_count]['links'][1]['href']
+                except IndexError:
+                    url = rss_d.entries[feed_count]['link']
+                if RSS_COMMAND is not None:
+                    feed_msg = f"{RSS_COMMAND} {url}"
+                else:
+                    feed_msg = f"<b>Name: </b><code>{rss_d.entries[feed_count]['title'].replace('>', '').replace('<', '')}</code>\n\n"
+                    feed_msg += f"<b>Link: </b><code>{url}</code>"
+                sendRss(feed_msg, context.bot)
+                feed_count += 1
+                sleep(5)
+            DbManager().rss_update(name, str(last_link), str(last_title))
+            with rss_dict_lock:
+                rss_dict[name] = [data[0], str(last_link), str(last_title), data[3]]
+            LOGGER.info(f"Feed Name: {name}")
+            LOGGER.info(f"Last item: {last_link}")
         except Exception as e:
-            LOGGER.error(f"Error monitoring feed: {name} - Error: {str(e)}")
-            
-def run_rss_monitor():
-    rss_monitor(context, db_url)
+            LOGGER.error(f"{e} Feed Name: {name} - Feed Link: {data[0]}")
+            continue
 
 if DB_URI is not None and RSS_CHAT_ID is not None:
     rss_list_handler = CommandHandler(BotCommands.RssListCommand, rss_list, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
@@ -338,8 +250,5 @@ if DB_URI is not None and RSS_CHAT_ID is not None:
     dispatcher.add_handler(rss_unsub_handler)
     dispatcher.add_handler(rss_settings_handler)
     dispatcher.add_handler(rss_buttons_handler)
-    partial_rss_monitor = lambda: rss_monitor(context, db_url)
-    rss_job = job_queue.run_repeating(run_rss_monitor, interval=RSS_DELAY, first=20, name="RSS")        
-            
-
-
+    rss_job = job_queue.run_repeating(rss_monitor, interval=RSS_DELAY, first=20, name="RSS")
+    rss_job.enabled = True
